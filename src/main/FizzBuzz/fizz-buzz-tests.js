59b1fb0f78ebf4993ef21495ee58ba808a47987f
duongtiendat1990 <48908567+duongtiendat1990@users.noreply.github.com>
2019-06-01 10:29:34 +0700

    "Reset"

--- src/main/FizzBuzz/fizz-buzz-tests.js
+++ src/main/FizzBuzz/fizz-buzz-tests.js
@@ -1,13 +1,3 @@
-
-QUnit.test( "fizz test", function( assert ) {
-  let number = 3;
-  let expected = "FIZZ";
-  let actual = fizzBuzz(number);
-  assert.equal( expected,actual, "Passed!" );
-});
-QUnit.test( "number test", function( assert ) {
-  let number = 4;
-  let expected = 4;
-  let actual = fizzBuzz(number);
-  assert.equal( expected,actual, "Passed!" );
+QUnit.test( "hello test", function( assert ) {
+  assert.ok( 1 == "1", "Passed!" );
 });
--- src/main/FizzBuzz/fizz-buzz.js
+++ src/main/FizzBuzz/fizz-buzz.js
@@ -1,9 +1,1 @@
-function fizzBuzz (number){
-  if (number === 3) {
-    return "FIZZ";
-  }
-  if (number === 4) {
-    return "4";
-  }
-}
 
